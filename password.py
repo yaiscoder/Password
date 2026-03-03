652636e1e982ac3b10d96af956cdd33458c179ee
Yaila Ustate C <yailauc.19@gmail.com>
2026-03-03 10:31:34 -0500

    cambios en el archivo

--- password.py
+++ password.py
@@ -1,15 +1,18 @@
-# Variables
-mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numeros = "0123456789"
-c_especial = "_$@*!"
+#Variables
 
 t_mayuscula = False
 t_numero = False
 t_c_especial = False
+arroba = False
+
+password = input("Ingrese su contraseña por favor: ")
+correo = input("Ingrese su correo")
 
 #Programa
 
-password = input("Ingrese su contraseña por favor: ")
+for email in correo:
+    if email == "@" and ".":
+        arroba = True
 
 while len (password) < 8:
     print("La contraseña debe tener minimo 8 carcateres")
@@ -21,11 +24,11 @@ while len (password) < 8:
         break
 
 for codigo in password:
-    if codigo in mayusculas:
+    if codigo.isupper():
         t_mayuscula = True
-    if codigo in numeros:
+    if codigo.isnumeric():
         t_numero = True
-    if codigo in c_especial:
+    if codigo.isalnum():
         t_c_especial = True
 
 if t_mayuscula and t_numero and t_c_especial:
@@ -33,6 +36,6 @@ if t_mayuscula and t_numero and t_c_especial:
 else:
     print("Su contraseña debe tener:")
     print("Minimo 8 caracteres")
-    print("1 mayúscula")
-    print("1 número")
-    print("1 carácter especial")
+    print("Minimo 1 mayúscula")
+    print("Minimo 1 número")
+    print("Minimo 1 carácter especial")
